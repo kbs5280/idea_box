82162fc490500c85f154f52da35464b5d1f08130
Kris Sparks <kbs5280@gmail.com>
2016-10-04 19:35:18 -0600

    Working on test bugs

--- spec/requests/api/v1/ideas_controller_sends_ideas_spec.rb
+++ spec/requests/api/v1/ideas_controller_sends_ideas_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 include RequestHelper
 describe 'Ideas API controller' do
   it 'sends a list of ideas' do
+    first_idea = create(:idea, title: 'Test Title One', body: 'Test idea one')
     ideas = create_list(:idea, 10)
 
     get '/api/v1/ideas'
 
     expect(response).to be_success
 
-    expect(json.first[:title]).to eq ideas.first.title
+    expect(json.first[:title]).to eq first_idea.title
+    expect(json.first[:body]).to eq first_idea.body
   end
 end
