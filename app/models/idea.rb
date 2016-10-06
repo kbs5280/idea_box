d6cd634b233a5f454119ed899f8cf10ad41a059a
Kris Sparks <kbs5280@gmail.com>
2016-10-06 06:46:50 -0600

    Refactored logic in model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -5,25 +5,28 @@ class Idea < ApplicationRecord
       idea.update(title: params[:idea][:title])
     elsif params[:idea][:body]
       idea.update(body: params[:idea][:body])
-
     elsif params[:idea][:vote] == "upvote"
-
-      if idea.quality == 'genius'
-      elsif idea.quality == 'plausible'
-        idea.update(quality: 'genius')
-      elsif idea.quality == 'swill'
-        idea.update(quality: 'plausible')
-      end
-
+      check_upvote(idea)
     elsif params[:idea][:vote] == "downvote"
+      check_downvote(idea)
+    end
+  end
 
-      if idea.quality == 'swill'
-      elsif idea.quality == 'plausible'
-        idea.update(quality: 'swill')
-      elsif idea.quality == 'genius'
-        idea.update(quality: 'plausible')
-      end
+  def self.check_upvote(idea)
+    if idea.quality == 'genius'
+    elsif idea.quality == 'plausible'
+      idea.update(quality: 'genius')
+    elsif idea.quality == 'swill'
+      idea.update(quality: 'plausible')
+    end
+  end
 
+  def self.check_downvote(idea)
+    if idea.quality == 'swill'
+    elsif idea.quality == 'plausible'
+      idea.update(quality: 'swill')
+    elsif idea.quality == 'genius'
+      idea.update(quality: 'plausible')
     end
   end
 end
