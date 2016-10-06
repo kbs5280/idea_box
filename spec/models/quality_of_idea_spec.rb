6ccc931b37099385123c5e36480eb5230bde3fd7
Kris Sparks <kbs5280@gmail.com>
2016-10-06 11:30:02 -0600

    Final testing complete

--- spec/models/quality_of_idea_spec.rb
+++ spec/models/quality_of_idea_spec.rb
@@ -36,4 +36,22 @@ describe 'Vote Idea', type: :model do
 
     expect(idea.quality).to eq 'plausible'
   end
+
+  it 'does not change idea quality based on downvote if already swill' do
+    idea = create(:idea, title: 'Test Title One', body: 'Test idea one', quality: 'swill')
+    params = { idea:{ vote: "downvote" }}
+
+    Idea.check_params(params, idea)
+
+    expect(idea.quality).to eq 'swill'
+  end
+
+  it 'does not update idea quality based on upvote if already genius' do
+    idea = create(:idea, title: 'Test Title One', body: 'Test idea one', quality: 'genius')
+    params = { idea:{ vote: "upvote" }}
+
+    Idea.check_params(params, idea)
+
+    expect(idea.quality).to eq 'genius'
+  end
 end
