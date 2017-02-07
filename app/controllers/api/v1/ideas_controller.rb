a801ba205b4bae2c2c94b3c62bdcf00fecb934f2
Kris Sparks <kbs5280@gmail.com>
2017-02-07 16:46:38 -0700

    Fix update action to return json object

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -9,17 +9,22 @@ class Api::V1::IdeasController < ApplicationController
     if idea.save
       respond_with(idea, location: api_v1_ideas_path)
     else
-      respond_with :json => [{ :error => "An error was encountered while processing your photos. Please try again." }], :status => 304
+      respond_with :json => [{ :error => "An error was encountered while
+                                          processing your photos.
+                                          Please try again."
+                           }], :status => 304
     end
   end
 
   def update
     idea = Idea.find(params[:id])
     Idea.check_params(params, idea)
+    respond_with idea, json: idea
   end
 
   def destroy
-    item = Idea.destroy(params[:id])
+    #remove variable idea
+    idea = Idea.destroy(params[:id])
   end
 
   private
